275241e2b093533d10919b31f326a0c7273d4cc3
telthhealth-cloud <telthhealth@gmail.com>
2026-04-30 14:34:16 +0530

    comited after google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+
 import Index from "./pages/Index";
 import CareHome from "./pages/CareHome";
 import PersonalizedCare from "./pages/PersonalizedCare";
@@ -18,30 +20,49 @@ import Refundpolicies from "./components/tabs/Refundpolicies";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/care-home" element={<CareHome />} />
-          <Route path="/personalized" element={<PersonalizedCare />} />
-          <Route path="/care-pay" element={<CarePay />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/careers" element={<Careers />} />
-          <Route path="/global-presence" element={<GlobalPresence />} />
-          <Route path="/terms-and-conditions" element={<TermsAndConditions />}/> 
-          <Route path="/privacy-policies" element={<PrivacyPolicy />}/>  
-          <Route path="/refund-policies" element={<Refundpolicies/>}/>  
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+function PageTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (window.gtag) {
+      window.gtag("config", "G-LM6K4R16WF", {
+        page_path: location.pathname,
+      });
+    }
+  }, [location]);
+
+  return null;
+}
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+
+        <BrowserRouter>
+          <PageTracker /> {/* ✅ correct placement */}
+
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/care-home" element={<CareHome />} />
+            <Route path="/personalized" element={<PersonalizedCare />} />
+            <Route path="/care-pay" element={<CarePay />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/careers" element={<Careers />} />
+            <Route path="/global-presence" element={<GlobalPresence />} />
+            <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+            <Route path="/privacy-policies" element={<PrivacyPolicy />} />
+            <Route path="/refund-policies" element={<Refundpolicies />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
